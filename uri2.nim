d97026f49cdaaab9be8d6636981d289531b4f870
achesak <achesak@yahoo.com>
2015-02-17 20:06:12 -0600

    Code complete.

--- uri2.nim
+++ uri2.nim
@@ -68,11 +68,208 @@ proc prependPathSegment*(uri : URI2, path : string) {.noreturn.} =
     
     newPath = path2 & "/" & newPath
     uri.path = newPath
-        
-    
 
-var test : URI2 = parseURI2("http://www.google.com/index.html?test=my%20data&test2=something1234")
-echo(test.path)
-test.prependPathSegment("testing")
-echo(test.path)
+
+proc getDomain*(uri : URI2): string = 
+    ## Returns the domain of ``uri``.
+    
+    return uri.hostname
+
+
+proc getScheme*(uri : URI2): string = 
+    ## Returns the scheme of ``uri``.
+    
+    return uri.scheme
+
+
+proc getUsername*(uri : URI2): string = 
+    ## Returns the username of ``uri``.
+    
+    return uri.username
+
+
+proc getPassword*(uri : URI2): string = 
+    ## Returns the password of ``uri``.
+    
+    return uri.password
+
+
+proc getPort*(uri : URI2): string = 
+    ## Returns the port of ``uri``.
+    
+    return uri.port
+
+
+proc getPath*(uri : URI2): string = 
+    ## Returns the path of ``uri``.
+    
+    return uri.path
+
+
+proc getPathSegments*(uri : URI2): seq[string] = 
+    ## Returns the path segments of ``uri`` as a sequence.
+    
+    var paths : seq[string] = uri.path.split("/")
+    
+    return paths[1..high(paths)]
+
+
+proc getPathSegment*(uri : URI2, index : int): string = 
+    ## Returns the path segment of ``uri`` at the specified index.
+    
+    return uri.getPathSegments()[index]
+
+
+proc getAnchor*(uri : URI2): string = 
+    ## Returns the anchor of ``uri``.
+    
+    return uri.anchor
+
+
+proc getAllQueries*(uri : URI2): seq[seq[string]] = 
+    ## Returns all queries of ``uri``.
+    
+    return uri.queries
+
+
+proc getQuery(uri : URI2, query : string, default : string = ""): string = 
+    ## Returns a specific query in ``uri``, or the specified ``default`` if there is no query with that name.
+    
+    var result : string = default
+    for i in uri.queries:
+        if i[0] == query:
+            result = i[1]
+            break
+    
+    return result
+
+
+proc setDomain*(uri : URI2, domain : string) {.noreturn.} =
+    ## Sets the domain of ``uri``.
+    
+    uri.hostname = domain
+
+
+proc setScheme*(uri : URI2, scheme : string) {.noreturn.} = 
+    ## Sets the scheme of ``uri``.
+    
+    uri.scheme = scheme
+
+
+proc setUsername*(uri : URI2, username : string) {.noreturn.} = 
+    ## Sets the username of ``uri``.
+    
+    uri.username = username
+
+
+proc setPassword*(uri : URI2, password : string) {.noreturn.} = 
+    ## Sets the password of ``uri``.
+    
+    uri.password = password
+
+
+proc setPort*(uri : URI2, port : string) {.noreturn.} = 
+    ## Sets the port of ``uri``.
+    
+    uri.port = port
+
+
+proc setPath*(uri : URI2, path : string) {.noreturn.} = 
+    ## Sets the path of ``uri``.
+    
+    uri.path = path
+
+
+proc setPathSegments*(uri : URI2, paths : seq[string]) {.noreturn.} = 
+    ## Sets the path segments of ``uri``.
+    
+    var newpath : string = ""
+    for i in 0..high(paths):
+        newpath &= "/" & paths[i]
+    
+    uri.path = newpath
+
+
+proc setPathSegment*(uri : URI2, path : string, index : int) {.noreturn.} = 
+    ## Sets the path segment of ``uri`` at the given index. If the given index is larger than the highest
+    ## current index, there will be no change.
+    
+    var segments : seq[string] = uri.getPathSegments()
+    if high(segments)  < index:
+        return
+    
+    segments[index] = path
+    uri.setPathSegments(segments)
+
+
+proc setAnchor*(uri : URI2, anchor : string) {.noreturn.} = 
+    ## Sets the anchor of ``uri``.
+    
+    uri.anchor = anchor
+
+
+proc setAllQueries(uri : URI2, queries : seq[seq[string]]) {.noreturn.} = 
+    ## Sets all the queries for ``uri``.
+    
+    uri.queries = queries
+
+
+proc setQuery(uri : URI2, query : string, value : string, overwrite : bool = true) {.noreturn.} = 
+    ## Sets the query with the specified name and value in ``uri``. If ``overwrite`` is set to false, this will not
+    ## overwrite any query with the same name that is already present.
+    
+    if not overwrite and uri.getQuery(query) != "":
+        return
+    
+    var exists : bool = false
+    var index : int = -1
+    for i in 0..high(uri.queries):
+        if uri.queries[i][0] == query:
+            exists = true
+            index = i
+            break
+    
+    if exists:
+        uri.queries[index][1] = value
+    else:
+        uri.queries.add(@[query, value])
+
+
+proc setQueries(uri : URI2, queryList : seq[seq[string]], overwrite : bool = true) {.noreturn.} = 
+    ## Sets multiple queries with the specified names and values in ``uri``. If ``overwrite`` is set to false, this will not
+    ## overwrite any query with the same name that is already present.
+    ##
+    ## This proc differs from ``setAllQueries()`` in that it does not remove any existing queries, but instead
+    ## just appends the new ones.
+    
+    for i in queryList:
+        uri.setQuery(i[0], i[1], overwrite)
+
+
+proc `/`*(uri : URI2, path : string) {.noreturn.} = 
+    ## Operator version of ``appendPathSegment()``.
+    
+    uri.appendPathSegment(path)
+
+
+proc `/`*(path : string, uri : URI2) {.noreturn.} = 
+    ## Operator version of ``prependPathSegment()``.
+    
+    uri.prependPathSegment(path)
+
+
+proc `$`*(uri : URI2): string = 
+    ## Convers ``uri`` to a string representation.
+    
+    var query : string = ""
+    for i in 0..high(uri.queries):
+        query &= uri.queries[i][0] & "=" & uri.queries[i][1]
+        if i != high(uri.queries):
+            query &= "&"
+    
+    # Let's be lazy about this. :P
+    var u : URI = URI(scheme: uri.scheme, username: uri.username, password: uri.password, hostname: uri.hostname,
+                      port: uri.port, path: uri.path, query: query, anchor: uri.anchor)
+    
+    return $u
 
